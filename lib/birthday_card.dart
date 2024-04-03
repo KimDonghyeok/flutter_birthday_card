d6c20c5ad00044d0fc432e3d91d0e0895b2c9a61
KimDonghyeok <qoophon@gmail.com>
2024-04-03 18:59:26 +0900

    modify: 생일축하 카드 위젯 수정

--- lib/birthday_card.dart
+++ lib/birthday_card.dart
@@ -8,30 +8,33 @@ class BirthdayCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
+    // 기본동작이 parent 를 fit 하게 expend 함
+    return Container(
+      padding: const EdgeInsets.all(24.0),
+      // Column 은 기본적으로 부모의 height 에 fit 하게 expend 한다.
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             'Happy',
             style: TextStyle(fontSize: 72),
           ),
-          Text(
+          const Text(
             'Birthday',
             style: TextStyle(fontSize: 72),
           ),
           Text(
             '$to!',
-            style: TextStyle(fontSize: 72),
+            style: const TextStyle(fontSize: 72),
           ),
+          // Row 은 기본적으로 부모의 wdith 에 fit 하게 expend 한다.
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Text(
                 'from $from',
-                style: TextStyle(fontSize: 24),
+                style: const TextStyle(fontSize: 24),
               ),
             ],
           )
